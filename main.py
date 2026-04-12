d61249bbaa97538176bfa66de283d1997be6935b
kanugupta2010 <kanugupta2010@gmail.com>
2026-04-13 00:10:27 +0530

    Add rewards domain to CORS

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://rewards.saraswatiretail.com",
+        "http://rewards.saraswatiretail.com",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
